8778727b1ad95cb7a671a9b6efa5d4e4ccbde30e
Zarpelon <100882885+Zarpelon616@users.noreply.github.com>
2023-06-26 20:11:08 -0300

    Create Veterinaro.cs

---
+++ Veterinaro.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arquivos.Model
+{
+    public class Veterinario
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? CPF { get; set; }
+        public string Email { get; set; }
+
+        public Veterinario()
+        {
+
+        }
+
+
+        public Veterinario(
+            int id
+            , string? firstName
+            , string? lastName
+            , string? cPF
+            , string? email
+        )
+        {
+            ID = id;
+            FirstName = firstName;
+            LastName = lastName;
+            CPF = cPF;
+            Email = email;
+        }
+    }
+}
