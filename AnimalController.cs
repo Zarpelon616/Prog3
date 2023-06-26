e43f34bec54366c705c23b3cdb002a284ca31633
Zarpelon <100882885+Zarpelon616@users.noreply.github.com>
2023-06-26 20:13:38 -0300

    Create AnimalController.cs

---
+++ AnimalController.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Arquivos.Data;
+using Arquivos.Model;
+
+namespace Arquivos.Controllers
+{
+    public class AnimalController
+    {
+        private string directoryName = "ReportFiles";
+        private string fileName = "Animal.txt";
+
+        public List<Animal> List()//vai trazer uma lista Animal
+        {
+            return DataSet.Animals;
+        }
+
+
+        //Bool é boleano
+        public bool Insert(Animal animal)//
+        {
+            if( animal == null)
+                return false;
+
+            if( string.IsNullOrWhiteSpace(animal.FirstName) )//IsNullOsWhiteSpace é quando esta vazio
+                return false;
+            
+            DataSet.Animals.Add(animal);
+            
+            return true;//Se passa pelas outras opções retora true
+        }
+
+         public bool ExportToTextFile()
+        {
+            if(!Directory.Exists(directoryName))//o ! = not faz ele retornar falso se não existir
+             Directory.CreateDirectory(directoryName);
+            
+            string fileContent = string.Empty;
+            foreach(Animal c in DataSet.Animals)// pega os clientes do dataset com a variavel c
+            {
+                fileContent +=$"{c.ID};{c.FirstName};{c.Email};{c.Tipo}";
+                fileContent += "\n";
+            }
+
+        try
+        {
+            StreamWriter sw = File.CreateText( $"{directoryName}\\{fileName}" );
+
+            sw.Write(fileContent);
+            sw.Close();
+        }
+        catch(IOException ioEx)//caso ocorra algum erro dentro desso processo vai agir
+        {
+            Console.WriteLine("Erro ao manipular o arquivo.");
+            Console.WriteLine(ioEx.Message);
+            return false;
+        }
+
+        return true;
+        }
+
+        public bool ImportFromTxtFile()
+        {
+            try
+            {
+                StringReader sr = new StringReader($"{directoryName}\\{fileName}");
+
+                string line = string.Empty;
+                line = sr.ReadLine();
+                while(line != null)//!= significa 'diferente'//significa enquanto line for diferente de null ele segue em frente
+                {
+                    Animal animal = new Animal();
+                    string[] animalData = line.Split(';');//cria um vetor de string usando[]//o Split que o line//usa aspas simples para caracteres 
+                    animal.ID = Convert.ToInt32( animalData[0] );//o [] é usado para determinar a string
+                    animal.FirstName = animalData[1];
+                    animal.Email = animalData[2];
+                    animal.Tipo = animalData[3];
+
+                    DataSet.Animals.Add(animal);
+
+                    line = sr.ReadLine(); //Vai fazer ele ler cada linha            
+                }
+
+                return true;
+            }
+            catch(Exception ex)//ex é exeption
+            {
+                Console.WriteLine("Oooops. Ocorreu um erro ao tentar importar os dados!");
+                Console.WriteLine(ex.Message);//ex.Message vai emviar uma mensagem
+                return false;
+            }
+
+
+            
+
+        }
+
+       
+        public int GetNextId() //int retorna um inteiro e GetNextId tras o proximo o id
+        {
+            int tam = DataSet.Animals.Count;
+            if( tam > 0 )
+                return DataSet.Animals[tam -1].ID + 1;
+            else
+                return 1;
+        }
+
+    }
+}
