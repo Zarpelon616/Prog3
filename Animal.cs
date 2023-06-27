30232f6b28fae3a9e7b7b43b154d5810c6289d9a
Zarpelon <100882885+Zarpelon616@users.noreply.github.com>
2023-06-26 21:11:59 -0300

    Update Animal.cs

--- Animal.cs
+++ Animal.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Arquivos.Model
-{//Inicio do Escopo do Namespace
+{
     public class Animal
-    {//Inicio do Escopo da Classe
-        public int ID { get; set; } //Atributo Unico
+    {
+        public int ID { get; set; } 
         public string FirstName { get; set; }
         public string Email { get; set; }
         public string Tipo { get; set; }
@@ -15,20 +15,20 @@ namespace Arquivos.Model
         public Animal()
         {
             
-        }//Permite usar a variavel sem criar valor nenhum de 'id'
+        }
 
-        //Método SEMPRE utiliza ()
+        
         public Animal(
             int id
             , string? firstName
             , string? email
             , string? tipo
         )
-        {//Inicio do Escopo deste método
+        {
             ID = ID;
             FirstName = firstName;
             Email = email;
             Tipo = tipo;
-        }//Fim do Escopo deste método
-    }//Fim do Escopo da Classe
-}//Fim do Escopo do Namespace
+        }
+    }
+}
