8e651b6becfc4675f03cfad4fa05329dff835a62
Zarpelon <100882885+Zarpelon616@users.noreply.github.com>
2023-06-26 21:19:37 -0300

    Update ClientController.cs

--- ClientController.cs
+++ ClientController.cs
@@ -98,11 +98,23 @@ namespace Arquivos.Controllers
                 return false;
             }
 
-
-            
-
         }
+        public List<Client> SearchByName(string name) //Procura pelo nome
+        {
+            if ( string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name) ) //Vai checar que se esta vazio
+            return null;
 
+            List<Client> clients = new List<Client>();
+            for(int i = 0; i < DataSet.Clients.Count; i++)
+            {
+                var c = DataSet.Clients[i]; //Percorre os elementos da lista e os coloca na variavel c
+                if( c.FullName.ToLower().Contains(name.ToLower()) ) //Tolower faz com que independente do maiusculo ou minusculo a busca funcione igual
+                {
+                    clients.Add(c);
+                }
+            }
+            return clients;
+        }
         public int GetNextId() //int retorna um inteiro e GetNextId tras o proximo o id
         {
             int tam = DataSet.Clients.Count;
